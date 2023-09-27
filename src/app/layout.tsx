d93e67b9622ed843018b6aafe5f694b99a018c1e
nico <nico@dwebsite.net>
2023-09-27 14:02:39 +0700

    add tailwind and daisyui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
+import './globals.css'
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,9 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <div>
-        {children}
-      </div>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
--- src/app/master/tag/[...tagId]/page.tsx
+++ src/app/master/tag/[...tagId]/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function TagDetail({params}: {params: {tagId: string}}) {
   return (
-    <div>Tag {params.tagId[2]}</div>
+    <div>detail :  {params.tagId[0]}</div>
   )
 }
